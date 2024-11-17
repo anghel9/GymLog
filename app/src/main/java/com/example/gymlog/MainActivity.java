17003eccf325da5044477e51c8196fd300bd4550
anghel9 <anghelcarrillo@gmail.com>
2024-11-16 23:04:08 -0800

    end of vid 8

--- app/src/main/java/com/example/gymlog/MainActivity.java
+++ app/src/main/java/com/example/gymlog/MainActivity.java
@@ -1,22 +1,33 @@
 package com.example.gymlog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gymlog.database.GymLogRepository;
+import com.example.gymlog.database.entities.GymLog;
 import com.example.gymlog.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    private static final String MAIN_ACTIVITY_USER_ID = "com.example.gymlog.MAIN_ACTIVITY_USER_ID";
+    private ActivityMainBinding binding;
+    private GymLogRepository repository;
 
     public static final String TAG = "DAC_GYMLOG";
     String mExercise = "";
     double mWeight = 0.0;
     int mReps = 0;
+
+    int loggedInUserId = -1;
+    //TODO add login
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +35,63 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
+        loginUser();
 
+        if(loggedInUserId == -1){
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
+
+        repository = GymLogRepository.getRepository(getApplication());
+        binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
+        updateDisplay();
         binding.logButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 getInformationFromDisplay();
+                insertGymlogRecord();
+                updateDisplay();
+            }
+        });
+
+        binding.exerciseInputEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 updateDisplay();
             }
         });
     }
 
+    private void loginUser() {
+        //TODO
+        loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID,-1);
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int userId){
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+        return intent;
+    }
+
+    private void insertGymlogRecord(){
+        if(mExercise.isEmpty()){
+            return;
+        }
+
+        GymLog log = new GymLog(mExercise,mWeight,mReps, loggedInUserId);
+        repository.insertGymLog(log);
+    }
+
     private void updateDisplay(){
-        String currentInfo = binding.logDisplayTextView.getText().toString();
-        Log.d(TAG,"current info: "+currentInfo);
-        String newDisplay = String.format(Locale.US,"Exercise:%s%nWeight:%.2f%nReps:%d%n=-=-=-=%n%s",mExercise,mWeight,mReps,currentInfo);
-        binding.logDisplayTextView.setText(newDisplay);
+        ArrayList<GymLog> allLogs = repository.getAllLogs();
+        if(allLogs.isEmpty()){
+            binding.logDisplayTextView.setText(R.string.nothing_to_show_go_gym);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(GymLog log : allLogs){
+            sb.append(log);
+        }
+        binding.logDisplayTextView.setText(sb.toString());
     }
 
     private void getInformationFromDisplay(){
